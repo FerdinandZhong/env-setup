4ad1515f8d2b4f2c0494df1252c1266dc52924fa
Qishuai <ferdinandzhong@gmail.com>
2022-09-28 10:51:36 +0800

    update env setup

--- py.sh
+++ py.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-CONDA="Miniconda3-latest-Linux-x86_64.sh"
+CONDA="Miniconda3-latest-MacOSX-x86_64.sh"
 
 # conda
 mkdir -p ${HOME}/Downloads
 [ -f "${HOME}/Downloads/${CONDA}" ] || wget -O ${HOME}/Downloads/${CONDA} "https://repo.anaconda.com/miniconda/${CONDA}"
 sh ${HOME}/Downloads/${CONDA} -b -u
 ${HOME}/miniconda3/bin/conda init
-[ -f "${HOME}/Downloads/${CONDA}" ] || touch ${HOME}/.bashrc
-. ${HOME}/.bashrc
+[ -f "${HOME}/Downloads/${CONDA}" ] || touch ${HOME}/.zshrc
+. ${HOME}/.zshrc
 
 # related libraries
 ${HOME}/miniconda3/bin/conda install -y -c conda-forge bat ripgrep exa fd-find
