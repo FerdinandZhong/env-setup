1714544f1a9bb7f55577e421fa5bd36d045efec8
root <ferdinandzhong@gmail.com>
2024-02-01 13:03:25 +0000

    update for vast

--- mamba.sh
+++ mamba.sh
@@ -6,4 +6,4 @@ MAMBA="Miniforge3-$(uname)-$(uname -m).sh"
 mkdir -p ${HOME}/Downloads
 
 curl -L "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" -o ${HOME}/Downloads/${MAMBA}
-bash ${HOME}/Downloads/Miniforge3-$(uname)-$(uname -m).sh -b -u
+bash ${HOME}/Downloads/Miniforge3-$(uname)-$(uname -m).sh
